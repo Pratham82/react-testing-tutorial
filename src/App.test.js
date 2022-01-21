3947d12e070f4802fad17cff40ca639e641b9585
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-01-22 02:16:52 +0530

    test: updated tescase

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Todo/i)
+  const linkElement = screen.getByText(/React Testing/i)
   expect(linkElement).toBeInTheDocument()
 })
