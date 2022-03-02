5097afe60f96c39c55440dca34888f6ee517ae3f
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-02 23:55:42 +0530

    test: added more test cases for counter and input

--- src/components/Counter/__test__/Counter.test.js
+++ src/components/Counter/__test__/Counter.test.js
@@ -1,10 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { getByTestId, render, screen } from '@testing-library/react'
 import Counter from '../Counter'
 
-describe('Tests for CounterApp', () => {
-  it('Checks if counter is present', () => {
-    render(<Counter />)
-    const headerEl = screen.getAllByTestId('header')
-    expect(headerEl).toBeTruthy()
-  })
+test('Checks if counter is present', () => {
+  render(<Counter />)
+  const headerEl = screen.getAllByTestId('header')
+  expect(headerEl).toBeTruthy()
+})
+
+test('Counter initially starts at 0', () => {
+  render(<Counter />)
+  const counterEl = screen.getByTestId('counter')
+  expect(counterEl.textContent).toBe('0')
+})
+
+test('Input contains initial value of 1', () => {
+  render(<Counter />)
+  const inputEl = screen.getByTestId('input')
+  expect(inputEl.value).toBe('1')
+})
+
+test('Add button renders with +', () => {
+  render(<Counter />)
+  const addBtn = screen.getByTestId('add-btn')
+  expect(addBtn.textContent).toBe('+')
+})
+
+test('Subtract button renders with  -', () => {
+  render(<Counter />)
+  const subtractBtn = screen.getByTestId('sub-btn')
+  expect(subtractBtn.textContent).toBe('-')
 })
