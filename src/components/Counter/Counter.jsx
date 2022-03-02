35e7a9cd4a190338ed5eb78d5012d14ed1d5493b
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-02 23:55:21 +0530

    feat: added counter and input values

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react'
+
 export default function Counter() {
+  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(1)
+
   return (
     <div>
-      <h2 data-testid="header">Counter App</h2>
+      <h1 data-testid="header">Counter App</h1>
+      <h2 data-testid="counter">{count}</h2>
+      <button data-testid="add-btn">+</button>
+      <input
+        data-testid="input"
+        type="number"
+        value={input}
+        style={{ textAlign: 'center' }}
+        onChange={e => setInput(e.target.value)}
+      />
+      <button data-testid="sub-btn">-</button>
     </div>
   )
 }
