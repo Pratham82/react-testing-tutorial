f63d6e09da9c7a018f686f814248f330de4932f8
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-05 17:36:54 +0530

    feat: added styles for counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './Counter.css'
 
 export default function Counter() {
   const [count, setCount] = useState(0)
@@ -7,16 +8,25 @@ export default function Counter() {
   return (
     <div>
       <h1 data-testid="header">Counter App</h1>
-      <h2 data-testid="counter">{count}</h2>
-      <button data-testid="add-btn">+</button>
+      <h2
+        data-testid="counter"
+        className={count >= 100 ? 'green' : count < 0 ? 'red' : ''}
+      >
+        {count}
+      </h2>
+      <button data-testid="add-btn" onClick={() => setCount(count + input)}>
+        +
+      </button>
       <input
         data-testid="input"
         type="number"
         value={input}
         style={{ textAlign: 'center' }}
-        onChange={e => setInput(e.target.value)}
+        onChange={e => setInput(Number(e.target.value))}
       />
-      <button data-testid="sub-btn">-</button>
+      <button data-testid="sub-btn" onClick={() => setCount(count - input)}>
+        -
+      </button>
     </div>
   )
 }
